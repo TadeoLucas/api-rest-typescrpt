2802a340521522399c0a7197cd896c61af0f085a
= <=>
2022-12-28 21:22:27 -0300

    docker compose up

--- src/schemas/config.schema.ts
+++ src/schemas/config.schema.ts
@@ -7,8 +7,13 @@ const configSchema = Joi.object({
     .allow('production')
     .default('development'),
   DB_HOST: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
   DB_PASS: Joi.string().required(),
-  DB_PORT: Joi.string().required()
+  DB_PORT: Joi.string().required(),
+  SERVER_PORT: Joi.number().default(3000),
+  JWT_SECRET: Joi.string().required(),
+  ES_NODE: Joi.string().required(),
+  DB_NAME_TEST: Joi.string()
 })
   .unknown()
   .required();
